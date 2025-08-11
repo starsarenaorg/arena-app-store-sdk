1c12109b5dbaa3d32498eb6972256be64c56fbfe
erdilalbayrak <ealbayrak@gmail.com>
2025-08-11 23:42:46 +0200

    Improve docs for the types

--- src/types/ArenaUserProfile.ts
+++ src/types/ArenaUserProfile.ts
@@ -1,10 +1,30 @@
+/**
+ * Represents a Arena user profile.
+ */
 export interface ArenaUserProfile {
+  /**
+   * Unique identifier for the Arena user.
+   */
   userId: string;
+
   /**
    * @deprecated Use userHandle instead.
+   * The handle of the Arena user.
    */
   username: string;
+
+  /**
+   * The handle of the Arena user.
+   */
   userHandle: string;
+
+  /**
+   * The display name of the Arena user.
+   */
   userName: string;
+
+  /**
+   * URL to the user's Arena profile image.
+   */
   userImageUrl: string;
 }
